37a87085d6e8b7c3c65f4f6176dafb008613cb87
Munif Gebara Junior <munifgebara@gmail.com>
2014-05-21 20:20:05 -0300

    Faltando outros TablesModels

--- src/br/com/fafiman/bcceventos/negocios/OperacoesBanco.java
+++ src/br/com/fafiman/bcceventos/negocios/OperacoesBanco.java
@@ -3,10 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package br.com.fafiman.bcceventos.negocios;
 
 import br.fafiman.bcceventos.BCCEventos;
+import br.fafiman.bcceventos.entidades.Evento;
+import br.fafiman.bcceventos.entidades.Inscricao;
 import br.fafiman.bcceventos.entidades.Participante;
 import java.util.List;
 import javax.persistence.EntityManager;
@@ -17,15 +18,23 @@ import javax.persistence.Query;
  * @author munifgebarajunior
  */
 public class OperacoesBanco {
-    
-    public static List<Participante> listaTodosParticipantes(){
-        EntityManager em=BCCEventos.emf.createEntityManager();
-        Query q=em.createQuery("from Participante p order by p.nome"); //HQL
+
+    public static List<Participante> listaTodosParticipantes() {
+        EntityManager em = BCCEventos.emf.createEntityManager();
+        Query q = em.createQuery("from Participante p order by p.nome"); //HQL
         return q.getResultList();
     }
-    
-    
-    
-    
-    
+
+    public static List<Evento> listaTodosEventos() {
+        EntityManager em = BCCEventos.emf.createEntityManager();
+        Query q = em.createQuery("from Evento e order by e.titulo"); //HQL
+        return q.getResultList();
+    }
+
+    public static List<Inscricao> listaTodasInscricoes() {
+        EntityManager em = BCCEventos.emf.createEntityManager();
+        Query q = em.createQuery("from Inscricao i order by i.dataRealizacao"); //HQL
+        return q.getResultList();
+    }
+
 }
